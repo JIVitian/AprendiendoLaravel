24a212b580298bbe17d6687c651f2db5bac49a0a
JIVitian <jivitian@gmail.com>
2021-08-13 12:16:10 -0300

    Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Curso::factory(50)->create();
 
         // Use the curso's seeders
-        $this->call(CursoSeeder::class);
+        // $this->call(CursoSeeder::class);
+
+        // In laravel 7, documentation recomend make seeders
+        // factory(App\User::class, 50)->create()->each(function ($user) {
+        //     $user->posts()->save(factory(App\Post::class)->make());
+        // });
     }
 }
