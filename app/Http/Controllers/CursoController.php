e2a24d38654b68082040465548888977137a94e5
JIVitian <jivitian@gmail.com>
2021-08-13 22:34:58 -0300

    Clase Request

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -4,34 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Curso;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCursoRequest;
 
 // Manage several routes
 class CursoController extends Controller
 {
     // Control the principal page
-    public function index() {
-        
+    public function index()
+    {
+
         // Take all cursos
         //$cursos = Curso::all();
         // Take 15 records
-        $cursos = Curso::orderBy('id','desc')->paginate();
+        $cursos = Curso::orderBy('id', 'desc')->paginate();
 
         // Use the variable named cursos
         return view('cursos.index', compact('cursos'));
     }
-    
+
     // Control the form page
-    public function create() {
+    public function create()
+    {
         return view('cursos.create');
     }
 
     // Control data storage
-    public function store(Request $request) {
-        $request->validate([
-            'name' => 'required|max:20',
-            'description' => 'required|min:10',
-            'category' => 'required'
-        ]);
+    public function store(StoreCursoRequest $request)
+    {
 
         $curso = new Curso();
 
@@ -43,17 +42,20 @@ class CursoController extends Controller
 
         return redirect()->route('cursos.show', $curso);
     }
-    
+
     // Control a particular page
-    public function show(Curso $curso) {
+    public function show(Curso $curso)
+    {
         return view('cursos.show', compact('curso'));
     }
 
-    public function edit(Curso $curso) {
+    public function edit(Curso $curso)
+    {
         return view('cursos.edit', compact('curso'));
     }
 
-    public function update(Request $request, Curso $curso) {
+    public function update(Request $request, Curso $curso)
+    {
         $request->validate([
             'name' => 'required',
             'description' => 'required',
